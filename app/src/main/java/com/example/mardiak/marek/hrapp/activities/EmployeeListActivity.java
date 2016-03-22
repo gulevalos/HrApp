48b75cf8eb6d24808694f81449ae885764b53c21
gulevalos <gulevalos@gmail.com>
2016-03-22 13:42:04 +0100

    added spinner

--- app/src/main/java/com/example/mardiak/marek/hrapp/activities/EmployeeListActivity.java
+++ app/src/main/java/com/example/mardiak/marek/hrapp/activities/EmployeeListActivity.java
@@ -8,14 +8,15 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
 import com.example.mardiak.marek.hrapp.JsonImporter;
@@ -28,22 +29,17 @@ import com.squareup.picasso.Picasso;
 /**
  * Created by mm on 3/9/2016.
  */
-public class EmployeeListActivity extends ListActivity
-        /*implements
-        LoaderManager.LoaderCallbacks<Cursor>*/
-{
+public class EmployeeListActivity extends ListActivity {
 
     private SimpleCursorAdapter employeesListAdapter;
-    //private SpinnerAdapter departmentSpinnerAdapter;
+    private SimpleCursorAdapter departmentSpinnerAdapter;
 
-    private LoaderManager.LoaderCallbacks<Cursor> employeeListLoaderListener
+    private LoaderManager.LoaderCallbacks<Cursor> departmentSpinnerLoaderListener
             = new LoaderManager.LoaderCallbacks<Cursor>() {
         @Override
         public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-            int departmentId = 1; //TODO set by spinner
-            String[] projection = { EmployeeTable.ID_COLUMN, EmployeeTable.LAST_NAME__COLUMN, EmployeeTable.FIRST_NAME__COLUMN, EmployeeTable.AVATAR__COLUMN };
-            Uri uri = ContentUris.withAppendedId(OrgStructureContentProvider.EMPLOYEES_BY_DEPARTMENT_ID_URI, departmentId);
-            //Uri uri = OrgStructureContentProvider.EMPLOYEES_ALL_URI;
+            String[] projection = {DepartmentTable.ID_COLUMN, DepartmentTable.NAME_COLUMN};
+            Uri uri = OrgStructureContentProvider.DEPARTMENTS_ALL_URI;
             CursorLoader cursorLoader = new CursorLoader(EmployeeListActivity.this,
                     uri, projection, null, null, null);
             return cursorLoader;
@@ -51,31 +47,45 @@ public class EmployeeListActivity extends ListActivity
 
         @Override
         public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-            employeesListAdapter.swapCursor(data);
+            departmentSpinnerAdapter.swapCursor(data);
         }
 
         @Override
         public void onLoaderReset(Loader<Cursor> loader) {
-            employeesListAdapter.swapCursor(null);
-        }};
-
-/*    private LoaderManager.LoaderCallbacks<Cursor> departmentSpinnerLoaderListener
-            = new LoaderManager.LoaderCallbacks<Cursor>() {
-        @Override
-        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-            return null;
+            departmentSpinnerAdapter.swapCursor(null);
         }
+    };
 
-        @Override
-        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+    private LoaderManager.LoaderCallbacks<Cursor> employeeListLoaderListener
+            = createEmployeeListLoaderListener(null);
 
-        }
+    @NonNull
+    private LoaderManager.LoaderCallbacks<Cursor> createEmployeeListLoaderListener(final Long departmentId) {
+        return new LoaderManager.LoaderCallbacks<Cursor>() {
+            @Override
+            public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+                String[] projection = {EmployeeTable.ID_COLUMN, EmployeeTable.LAST_NAME__COLUMN, EmployeeTable.FIRST_NAME__COLUMN, EmployeeTable.AVATAR__COLUMN};
+                Uri uri = OrgStructureContentProvider.EMPLOYEES_ALL_URI;
+                if (departmentId != null) {
+                    uri = ContentUris.withAppendedId(OrgStructureContentProvider.EMPLOYEES_BY_DEPARTMENT_ID_URI, departmentId);
+                }
+                CursorLoader employeesListCursorLoader = new CursorLoader(EmployeeListActivity.this,
+                        uri, projection, null, null, null);
+                return employeesListCursorLoader;
+            }
 
-        @Override
-        public void onLoaderReset(Loader<Cursor> loader) {
-            departmentSpinnerAdapter.
-        }
-    };*/
+            @Override
+            public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+                employeesListAdapter.swapCursor(data);
+            }
+
+            @Override
+            public void onLoaderReset(Loader<Cursor> loader) {
+                employeesListAdapter.swapCursor(null);
+            }
+        };
+    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -83,43 +93,43 @@ public class EmployeeListActivity extends ListActivity
         setContentView(R.layout.todo_list);
         this.getListView().setDividerHeight(2);
         fillEmployeeData();
-        //fillDepartmentData();
+        fillDepartmentData();
         registerForContextMenu(getListView());
     }
 
-/*    private void fillDepartmentData() {
-
+    private void fillDepartmentData() {
+        String[] from = new String[]{DepartmentTable.NAME_COLUMN};
+        departmentSpinnerAdapter = new SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_spinner_item,
+                null,
+                from,
+                new int[]{android.R.id.text1},
+                0);
+        departmentSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         Spinner spinner = (Spinner) this.findViewById(R.id.depSpinner);
-        departmentSpinnerAdapter = spinner.getAdapter();
+        spinner.setAdapter(departmentSpinnerAdapter);
 
-        String[] from = new String[] {DepartmentTable.ID_COLUMN, DepartmentTable.NAME_COLUMN };
-        int[] to = new int[] { R.id.person_avatar, R.id.person_label, R.id.person_content };
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                getLoaderManager().restartLoader(0, null, createEmployeeListLoaderListener(id)); //TODO better about of informing URI parameter change?
+            }
 
-        //getLoaderManager().initLoader(0, null, employeeListLoaderListener); //TODO 2 listeners one for empDataList second for Department spinner
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
         getLoaderManager().initLoader(1, null, departmentSpinnerLoaderListener);
-
-        new SpinnerAAdapter(this, R.layout.todo_row, null, from,
-                to, 0);
-
-        spinner.setAdapter();
-    }*/
+    }
 
     private void fillEmployeeData() {
-        String[] from = new String[] { EmployeeTable.ID_COLUMN, EmployeeTable.LAST_NAME__COLUMN, EmployeeTable.FIRST_NAME__COLUMN, EmployeeTable.AVATAR__COLUMN };
-        int[] to = new int[] { R.id.person_avatar, R.id.person_label, R.id.person_content };
+        String[] from = new String[]{EmployeeTable.ID_COLUMN, EmployeeTable.LAST_NAME__COLUMN, EmployeeTable.FIRST_NAME__COLUMN, EmployeeTable.AVATAR__COLUMN};
+        int[] to = new int[]{R.id.person_avatar, R.id.person_label, R.id.person_content};
 
         getLoaderManager().initLoader(0, null, employeeListLoaderListener); //TODO 2 listeners one for empDataList second for Department spinner
-        //getLoaderManager().initLoader(1, null, departmentSpinnerLoaderListener);
         employeesListAdapter = new SimpleCursorAdapter(this, R.layout.todo_row, null, from,
                 to, 0);
-/*        final Picasso picasso = new Picasso.Builder(this)
-                .listener(new Picasso.Listener() {
-                    @Override
-                    public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception) {
-                        String s="sss";
-                    }
-                })
-                .build();*/
         employeesListAdapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
             public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
                 switch (view.getId()) {
@@ -159,8 +169,5 @@ public class EmployeeListActivity extends ListActivity
         }
         return super.onOptionsItemSelected(item);
     }
-
-
-
 
 }
