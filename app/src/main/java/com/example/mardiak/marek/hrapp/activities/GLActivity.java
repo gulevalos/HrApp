489f72fbf7d1c925144cbdcadaa6b364239aafb4
gulevalos <gulevalos@gmail.com>
2016-03-24 01:59:40 +0100

    added drawer menu to GL view

--- app/src/main/java/com/example/mardiak/marek/hrapp/activities/GLActivity.java
+++ app/src/main/java/com/example/mardiak/marek/hrapp/activities/GLActivity.java
@@ -6,14 +6,29 @@ import android.content.Context;
 import android.content.pm.ConfigurationInfo;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
+import com.example.mardiak.marek.hrapp.R;
 import com.example.mardiak.marek.hrapp.myOpenGl.MyGLRenderer;
 import com.example.mardiak.marek.hrapp.myOpenGl.MyGlSurfaceView;
 
 /**
  * Created by mm on 3/23/2016.
  */
-public class GLActivity extends Activity {
+public class GLActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
 
     /** Hold a reference to our GLSurfaceView */
     private GLSurfaceView mGLSurfaceView;
@@ -46,7 +61,10 @@ public class GLActivity extends Activity {
             return;
         }
 
-        setContentView(mGLSurfaceView);
+
+        setContentView(R.layout.gl_layout);
+        ((FrameLayout)findViewById(R.id.gl_content_frame)).addView(mGLSurfaceView);
+        createDrawerMenu();
     }
 
     @Override
@@ -63,5 +81,59 @@ public class GLActivity extends Activity {
         // The activity must call the GL surface view's onPause() on activity onPause().
         super.onPause();
         mGLSurfaceView.onPause();
+    }
+
+    private void createDrawerMenu() {
+        // Initializing Toolbar and setting it as the actionbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Initializing NavigationView
+        navigationView = (NavigationView) findViewById(R.id.gl_navigation_view);
+
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if (menuItem.isChecked()) menuItem.setChecked(false);
+                else menuItem.setChecked(true);
+
+                //Closing drawer on item click
+                drawerLayout.closeDrawers();
+
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()) {
+                    case R.id.changeColor:
+                        Toast.makeText(getApplicationContext(), "importJson", Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        Toast.makeText(getApplicationContext(), "Somethings Wrong", Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+            }
+        });
+
+        // Initializing Drawer Layout and ActionBarToggle
+        drawerLayout = (DrawerLayout) findViewById(R.id.gl_drawer);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
     }
 }
